496a4ebd4a6345426eaf80121c23b8d2687b1690
Stefano Origgi <stefano.origgi@issgreppi.it>
2024-10-19 14:53:18 +0200

    Sat Oct 19 14:53:18 CEST 2024

---
+++ _INF/EsProva/ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Hello, World!");
+static void Metodo1(){
+    string x = Console.ReadLine();
+    if (x == "10")
+    {
+        System.Console.WriteLine("ciao mondo");
+    }
+    else
+        System.Console.WriteLine("Ciao mondo 2");
+}
+Metodo1();
