3ef98ea1d66b7ce560ce2b1bdec5157135890044
Stefano Origgi <stefano.origgi@issgreppi.it>
2024-11-04 08:40:58 +0100

    Mon Nov  4 08:40:58 CET 2024

---
+++ _INF/prova1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace prova1;
+class Program
+{
+    public static string ReverseString(string input)
+    {
+        char[] charArray = input.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+    static void Main(string[] args)
+    {
+        string reversed = ReverseString("example");
+        Console.WriteLine(reversed);
+    }  
+}
