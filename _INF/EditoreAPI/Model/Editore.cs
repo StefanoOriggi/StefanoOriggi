30556cd5912ff1fdd2d15872cd66252886263d6c
Stefano Origgi <stefano.origgi@issgreppi.it>
2025-01-10 13:30:58 +0100

    sistemo

--- _INF/EditoreAPI/Model/Editore.cs
+++
@@ -1,8 +0,0 @@
-namespace EditoreAPI.Model;
-public class Editore
-{
-    public int EditoreId { get; set; }
-    public string NomeEditore { get; set; }
-    public string citta { get; set; }
-    public List<Libro> Libri { get; set; }
-}
